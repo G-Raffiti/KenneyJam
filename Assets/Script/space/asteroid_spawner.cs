1ea74fb0c390704c99cbb93c23836bbdc71957df
maussang <brice.maussang@inrae.fr>
2023-07-22 19:17:38 +0200

    rebing

--- Assets/Script/space/asteroid_spawner.cs
+++ Assets/Script/space/asteroid_spawner.cs
@@ -5,12 +5,6 @@ namespace Script.space
 {
 	public class asteroid_spawner: MonoBehaviour
 	{
-		public asteroid prefab;
-
-		private 
-		private void Update()
-		{
-			throw new NotImplementedException();
-		}
+		
 	}
 }
