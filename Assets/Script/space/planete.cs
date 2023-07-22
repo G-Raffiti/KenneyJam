3784e97531d4888434cd55ce64555e80c5e704bb
G-Raffiti <69368335+G-Raffiti@users.noreply.github.com>
2023-07-22 22:30:42 +0200

    ping + urp

--- Assets/Script/space/planete.cs
+++ Assets/Script/space/planete.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class planete : MonoBehaviour
 {
@@ -8,18 +7,25 @@ public class planete : MonoBehaviour
     public float rotaionSpeedSelf;
 
     public GameObject pivotObject;
-    
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string planete_scene_name = "";
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(new Vector3(0,0,1), rotaionSpeedSelf * Time.deltaTime);
         transform.RotateAround(pivotObject.transform.position, new Vector3(0,0,1), rotaionSpeed * Time.deltaTime * 0.1f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            OpenScenePlanete();
+    }
+
+    private void OpenScenePlanete()
+    {
+        if (planete_scene_name == "")
+            return;
+        SceneManager.LoadScene(planete_scene_name, LoadSceneMode.Additive);
     }
 }
---
+++ Assets/Script/ui/ping.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ping : MonoBehaviour
+{
+    public planete planete;
+
+    public Transform player;
+
+    public SpriteRenderer sprite;
+    private bool fade = true;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float rotation = Mathf.Deg2Rad * player.rotation.eulerAngles.z;
+        Vector2 direction = planete.transform.position - player.position;
+
+        direction = Rotate(direction, -rotation);
+        float factor = 1;
+        if (Math.Abs(direction.y) > Math.Abs(direction.x))
+            factor = 7 / Math.Abs(direction.y);
+        else
+            factor = 12 / Math.Abs(direction.x);
+        transform.localPosition = direction * factor;
+
+        Color color = sprite.color;
+        
+        if (fade)
+        {
+            if (color.a < 0.1)
+                fade = !fade;
+            color = new Color(color.r, color.g, color.b, color.a - Time.deltaTime);
+        }
+        else
+        {
+            if (color.a > 0.8)
+                fade = !fade;
+            color = new Color(color.r, color.g, color.b, color.a + Time.deltaTime);
+        }
+
+        sprite.color = color;
+
+    }
+    
+    public static Vector2 Rotate(Vector2 v, float delta)
+    {
+        return new Vector2(
+            v.x * Mathf.Cos(delta) - v.y * Mathf.Sin(delta),
+            v.x * Mathf.Sin(delta) + v.y * Mathf.Cos(delta)
+        );
+    }
+}
