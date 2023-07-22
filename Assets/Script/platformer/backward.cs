b6b809923aea5ba8dbf694ba1271487e68b9b995
G-Raffiti <69368335+G-Raffiti@users.noreply.github.com>
2023-07-22 21:06:29 +0200

    platform indus

---
+++ Assets/Script/platformer/backward.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class backward : MonoBehaviour
+{
+    public TilemapCollider2D collider;
+    public playerPlatformController player;
+    private Collider2D player_collider; 
+    public bool left = true;
+
+    private void Start()
+    {
+        player_collider = player.GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        // if (collider.IsTouching(player_collider))
+        //     player.goBack(left ? -1 : 1);
+        // else if (!player.has_control)
+        //     player.has_control = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player") {
+            player.goBack(left ? -1 : 1);
+        }
+    }
+	
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player") {
+            player.has_control = true;
+        }
+    }
+}
