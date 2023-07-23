a6661a3a2d03cdfde7a85fa2eb43ff0ad3799296
maussang <brice.maussang@inrae.fr>
2023-07-23 11:40:38 +0200

    begin platformer

--- Assets/Script/platformer/bumper.cs
+++ Assets/Script/platformer/bumper.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+
+
 public class bumper : MonoBehaviour
 {
     public TilemapCollider2D collider;
